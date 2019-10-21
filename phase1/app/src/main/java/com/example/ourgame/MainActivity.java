438a7c8b7fcd62d3ebdf08112349cbfb61dae361
Artur <artur.kuramshin@mail.utoronto.ca>
2019-10-21 14:39:14 -0400

    Added buttons to main activity with back buttons

--- phase1/app/src/main/java/com/example/ourgame/MainActivity.java
+++ phase1/app/src/main/java/com/example/ourgame/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.ourgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playPictureGame(View view){
+        Intent intent = new Intent(this, PictureGame.class);
+        startActivity(intent);
+    }
+
+    public void playTileGameView (View view){
+        Intent intent = new Intent(this, TileGame.class);
+        startActivity(intent);
+    }
+
+    public void playReactionGame(View view){
+        Intent intent = new Intent(this, ReactionTime.class);
+        startActivity(intent);
     }
 }
