b1f214538189a2efff367dc5fce4120c2ad73f0e
brady <bradyhuai@outlook.com>
2019-10-29 17:30:19 -0400

    Made changes to each tile to display their colour before changing to the default hidden colour.

--- phase1/app/src/main/java/com/example/ourgame/TileGame.java
+++ phase1/app/src/main/java/com/example/ourgame/TileGame.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -37,6 +38,7 @@ public class TileGame extends AppCompatActivity implements View.OnClickListener 
                             R.drawable.flipped_right,
                             R.drawable.flipped_right));
 
+
     private int[] buttons = {
             R.id.button1,
             R.id.button2,
@@ -54,21 +56,13 @@ public class TileGame extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_tile_game);
         countLives = findViewById(R.id.lives);
 
-        TextView text = findViewById(R.id.title);
-
         Collections.shuffle(images);
 
         for (int i = 0; i < buttons.length; i++) {
             Button button = findViewById(buttons[i]);
-            //button.setTextSize(0.0F);
+            button.setTextSize(0.0F);
             button.setOnClickListener(this);
 
-            if (images.get(i) == R.drawable.flipped_wrong) {
-                button.setText("wrong");
-
-            } else {
-                button.setText("right");
-            }
         }
 
         findViewById(R.id.button10)
@@ -80,6 +74,34 @@ public class TileGame extends AppCompatActivity implements View.OnClickListener 
                                 finish();
                             }
                         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Collections.shuffle(images);
+
+        for (int i = 0; i < buttons.length; i++) {
+            final Button button = findViewById(buttons[i]);
+            button.setTextSize(0.0F);
+
+
+            if (images.get(i) == R.drawable.flipped_wrong) {
+                button.setText("wrong");
+                button.setBackgroundResource(R.drawable.flipped_wrong);
+
+            } else {
+                button.setText("right");
+                button.setBackgroundResource(R.drawable.flipped_right);
+            }
+
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    button.setBackgroundResource(R.drawable.unflipped);
+                }
+            }, 5000);
+        }
     }
 
     @Override
@@ -119,8 +141,9 @@ public class TileGame extends AppCompatActivity implements View.OnClickListener 
         Collections.shuffle(images);
 
         for (int i = 0; i < buttons.length; i++) {
-            Button button = findViewById(buttons[i]);
-            //button.setTextSize(0.0F);
+            final Button button = findViewById(buttons[i]);
+            button.setTextSize(0.0F);
+
 
             if (images.get(i) == R.drawable.flipped_wrong) {
                 button.setText("wrong");
@@ -130,12 +153,13 @@ public class TileGame extends AppCompatActivity implements View.OnClickListener 
                 button.setText("right");
                 button.setBackgroundResource(R.drawable.flipped_right);
             }
-        }
 
-        for (int button : buttons) {
-            findViewById(button).setBackgroundResource(R.drawable.unflipped);
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    button.setBackgroundResource(R.drawable.unflipped);
+                }
+            }, 3000);
         }
-
     }
 
 }
