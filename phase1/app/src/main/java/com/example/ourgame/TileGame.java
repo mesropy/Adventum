8e4b04637cc2dc4c269d155472ba7798242a64f8
Ethan <ethan.ku@mail.utoronto.ca>
2019-10-29 17:11:57 -0400

    Implemented the basic logic of the game to make it playable.

--- phase1/app/src/main/java/com/example/ourgame/TileGame.java
+++ phase1/app/src/main/java/com/example/ourgame/TileGame.java
@@ -1,5 +1,5 @@
 package com.example.ourgame;
-//
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,9 +12,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * The main Activity class for a Memory Tile Game
+ */
 public class TileGame extends AppCompatActivity implements View.OnClickListener {
 
-  private int lives = 3;
+    private int lives = 3;
+    private int roundLives = 2;
+    private int points = 0;
+    private int correctPressed = 0;
+
+    private TextView countLives;
 
     private ArrayList<Integer> images =
             new ArrayList<>(
@@ -29,71 +37,105 @@ public class TileGame extends AppCompatActivity implements View.OnClickListener 
                             R.drawable.flipped_right,
                             R.drawable.flipped_right));
 
-    private int[] buttons = {R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5,R.id.button6, R.id.button7, R.id.button8, R.id.button9};
+    private int[] buttons = {
+            R.id.button1,
+            R.id.button2,
+            R.id.button3,
+            R.id.button4,
+            R.id.button5,
+            R.id.button6,
+            R.id.button7,
+            R.id.button8,
+            R.id.button9};
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_tile_game);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tile_game);
+        countLives = findViewById(R.id.lives);
 
-    TextView text = findViewById(R.id.title);
+        TextView text = findViewById(R.id.title);
 
-    Collections.shuffle(images);
+        Collections.shuffle(images);
 
-      for (int i = 0; i < buttons.length; i++) {
-          Button button = findViewById(buttons[i]);
-          button.setTextSize(0.0F);
-          button.setOnClickListener(this);
+        for (int i = 0; i < buttons.length; i++) {
+            Button button = findViewById(buttons[i]);
+            //button.setTextSize(0.0F);
+            button.setOnClickListener(this);
 
-          if (images.get(i) == R.drawable.flipped_wrong){
-              button.setText("wrong");
+            if (images.get(i) == R.drawable.flipped_wrong) {
+                button.setText("wrong");
 
-          }
-          else{
-              button.setText("right");}
-      }
-
-    findViewById(R.id.button10)
-        .setOnClickListener(
-            new View.OnClickListener() {
-              public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TileGame.class);
-                startActivity(intent);
-                finish();
-              }
-            });
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-  }
-
-  @Override
-  public void onClick(View view) {
-      Button button = (Button)view;
-      button.setBackgroundResource(images.get(getButtonIndex(button.getId())));
-  }
-
-    public int getButtonIndex(int t) {
-
-        // find length of array
-        int len = buttons.length;
-        int i = 0;
-
-        // traverse in the array
-        while (i < len) {
-
-            // if the i-th element is t
-            // then return the index
-            if (buttons[i] == t) {
-                return i;
-            }
-            else {
-                i = i + 1;
+            } else {
+                button.setText("right");
             }
         }
-        return -1;
+
+        findViewById(R.id.button10)
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            public void onClick(View view) {
+                                Intent intent = new Intent(getApplicationContext(), TileGame.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+
+        if (button.getText() == "wrong") {
+
+            button.setBackgroundResource(R.drawable.flipped_wrong);
+            roundLives--;
+
+            if (roundLives == 0) {
+                lives--;
+                countLives.setText("Lives Remaining: " + Integer.toString(lives) + "/3");
+                roundLives = 2;
+                restartGame();
+            }
+
+            if (lives == 0) {
+                //end activity
+            }
+        } else {
+            button.setBackgroundResource(R.drawable.flipped_right);
+            points++;
+            correctPressed++;
+
+            if (correctPressed == 4) {
+                roundLives = 2;
+                restartGame();
+                correctPressed = 0;
+            }
+        }
+    }
+
+    private void restartGame() {
+
+        Collections.shuffle(images);
+
+        for (int i = 0; i < buttons.length; i++) {
+            Button button = findViewById(buttons[i]);
+            //button.setTextSize(0.0F);
+
+            if (images.get(i) == R.drawable.flipped_wrong) {
+                button.setText("wrong");
+                button.setBackgroundResource(R.drawable.flipped_wrong);
+
+            } else {
+                button.setText("right");
+                button.setBackgroundResource(R.drawable.flipped_right);
+            }
+        }
+
+        for (int button : buttons) {
+            findViewById(button).setBackgroundResource(R.drawable.unflipped);
+        }
+
     }
 
 }
