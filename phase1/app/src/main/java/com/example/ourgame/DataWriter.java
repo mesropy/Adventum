811667604908a6481c3eb7731ffee888ef8449e5
Artur <artur.kuramshin@mail.utoronto.ca>
2019-10-30 14:59:36 -0400

    Fixed error with DataWriter not implementing required methods

--- phase1/app/src/main/java/com/example/ourgame/DataWriter.java
+++ phase1/app/src/main/java/com/example/ourgame/DataWriter.java
@@ -3,5 +3,53 @@ package com.example.ourgame;
 public class DataWriter implements WriteData {
 
 
+    @Override
+    public void addUser(String username, String password) {
 
+    }
+
+    @Override
+    public String getPassword(String username) {
+        return null;
+    }
+
+    @Override
+    public int[] getReactionStats(String username) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] getTileStats(String username) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] getPictureStats(String username) {
+        return new int[0];
+    }
+
+    @Override
+    public void setReactionStats(int[] stats) {
+
+    }
+
+    @Override
+    public void setTileStats(int[] stats) {
+
+    }
+
+    @Override
+    public void setPictureStats(int[] stats) {
+
+    }
+
+    @Override
+    public String getLastGame(String username) {
+        return null;
+    }
+
+    @Override
+    public boolean checkUser(String username) {
+        return false;
+    }
 }
