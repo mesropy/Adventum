fbe79e44d5a3e764cd4f19fb0ac7795d81a710ef
Anoushka P <anoushka.prasad@mail.utoronto.ca>
2019-11-01 00:40:09 -0400

    Implemented and improved part of display on StatisticsActivity page.

--- phase1/app/src/main/java/com/example/ourgame/StatisticsActivity.java
+++ phase1/app/src/main/java/com/example/ourgame/StatisticsActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    DataWriter dataWriter;
+
+    TextView pointsText;
+    TextView playtimeText;
+    TextView rankingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        pointsText = findViewById(R.id.pointsValue);
+        playtimeText = findViewById(R.id.playtimeValue);
+        rankingText = findViewById(R.id.rankingValue);
+
+        /*
+        dataWriter = new DataWriter(this);
+        pointsText.setText(dataWriter.getPoints(MainActivity.user));
+        playtimeText.setText(dataWriter.getPlayTime(MainActivity.user));
+        rankingText.setText(dataWriter.getRanking(MainActivity.user));*/
     }
 
     public void onContinuePressed(View view){
